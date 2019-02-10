2149a44bb74ad41cab362d2fb515a195a4fa68ef
ahtwq <ahtwq@163.com>
2019-02-10 17:07:09 +0800

    ip

--- getIp.sh
+++ getIp.sh
@@ -3,9 +3,8 @@
 
 while true
 do
-	date > ipadd.txt
-	echo "\n" >> ipadd.txt
-    curl ifconfig.me >> ipadd.txt
+	date > ipadd.txt	
+    curl v4.ifconfig.co >> ipadd.txt
     git add .
     git commit -m 'ip'
     git push -u origin master
